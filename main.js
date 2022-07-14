45cda2fa63ae1677b66b572b7db21eb406acdce3
Woo Jin Kim <effect105@hotmail.com>
2022-07-15 03:11:16 +0900

    added subcommands and an event handler

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
 require('dotenv').config();
-const { Client, Intents } = require('discord.js');
+const fs = require("fs");
+const path = require('node:path');
+const { Client, Intents, Collection } = require('discord.js');
 const token = process.env.DISCORD_TOKEN;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles){
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+for (const file of eventFiles){
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once){
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else{
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
+	if(!command) return;
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nCreated at: ${interaction.guild.createdAt}`);
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
+	try {
+		await command.execute(interaction);
 	}
+	catch(error){
+		console.error(error);
+		await interaction.reply('SOMETHING WENT WRONG');
+		console.log(interaction.commandId)
+	}
+	
 });
 
 client.login(token);
